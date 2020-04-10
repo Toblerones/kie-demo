c862a9bbfd15f856208fc51a6be4e6f17f16b9db
toblerones <toblerones@hotmail.com>
2020-04-10 21:55:05 +1000

    Project dependency. Application up.

---
+++ drools-rule-engine/src/main/java/cn/tobs/drools/DroolsRuleEngineApp.java
@@ -0,0 +1,12 @@
+package cn.tobs.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsRuleEngineApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsRuleEngineApp.class, args);
+    }
+}
---
+++ drools-rule-engine/src/main/java/cn/tobs/drools/controller/DroolsController.java
@@ -0,0 +1,4 @@
+package cn.tobs.drools.controller;
+
+public class DroolsController {
+}
