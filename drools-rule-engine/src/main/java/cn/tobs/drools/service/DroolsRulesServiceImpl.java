ce8555ead97bb0cb38ff8eaa11de2862e30b3387
toblerones <toblerones@hotmail.com>
2020-04-13 16:23:56 +1000

    Reload rule when check (Temp solution)

--- drools-rule-engine/src/main/java/cn/tobs/drools/service/DroolsRulesServiceImpl.java
+++ drools-rule-engine/src/main/java/cn/tobs/drools/service/DroolsRulesServiceImpl.java
@@ -72,6 +72,7 @@ public class DroolsRulesServiceImpl {
             rule.setRule(dreRule.getContent());
             rules.add(rule);
         }
+        reloadRulesFromDb();
         return rules;
     }
 
